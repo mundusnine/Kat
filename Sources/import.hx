fa2081032c9a485786b1b822d7554d081a8b8c27
mundusnine <mundusnine@gmail.com>
2021-06-22 10:57:04 -0400

    Make everything in C

--- Sources/import.hx
+++
@@ -1,7 +0,0 @@
-// Global imports
-
-#if (!macro)
-
-import kat.tool.Translator.tr;
-using StringTools;
-#end
--- Sources/kat/ui/View.hx
+++
@@ -1,9 +0,0 @@
-package kat.ui;
-
-import zui.Zui;
-import zui.Canvas.TElement;
-
-interface View {
-    public function render(ui:Zui,element:TElement):Void;
-    public function redraw():Void;
-}
